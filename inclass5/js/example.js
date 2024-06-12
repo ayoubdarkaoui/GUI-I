2e600fd3700481624e97fea53f6afde31c768215
Ayoub Darkaoui <70981013+ayoubdarkaoui@users.noreply.github.com>
2024-06-11 20:41:38 -0400

    Update example.js

--- inclass5/js/example.js
+++ inclass5/js/example.js
@@ -1,24 +1,23 @@
+// ADD NEW ITEM TO END OF LIST
+const list = document.querySelector('ul');
+const newItemEnd = document.createElement('li');
+newItemEnd.textContent = 'cream';
+list.appendChild(newItemEnd);
 
-    // Step 1: Add new item "cream" to end of list
-    const list = document.querySelector('ul');
-    const newItemEnd = document.createElement('li');
-    newItemEnd.textContent = 'cream';
-    list.appendChild(newItemEnd);
-  
-    // Step 2: Add new item "kale" to start of list
-    const newItemStart = document.createElement('li');
-    newItemStart.textContent = 'kale';
-    list.insertBefore(newItemStart, list.firstChild);
-  
-    // Step 3: Add a class of "cool" to all list items
-    const listItems = list.querySelectorAll('li');
-    listItems.forEach(item => item.classList.add('cool'));
-  
-    // Step 4: Add number of items in the list to the heading
-    const itemCount = listItems.length;
-    const heading = document.querySelector('h2');
-    const span = document.createElement('span');
-    span.textContent = `${itemCount}`;
-    heading.appendChild(span);
+// ADD NEW ITEM START OF LIST
+const newItemStart = document.createElement('li');
+newItemStart.textContent = 'kale';
+list.insertBefore(newItemStart, list.firstChild);
+
+// ADD A CLASS OF COOL TO ALL LIST ITEMS
+const listItems = list.querySelectorAll('li');
+listItems.forEach(item => item.classList.add('cool'));
+
+// ADD NUMBER OF ITEMS IN THE LIST TO THE HEADING
+const itemCount = listItems.length;
+const heading = document.querySelector('h2');
+const span = document.createElement('span');
+span.textContent = `${itemCount}`;
+heading.appendChild(span);
   
   
